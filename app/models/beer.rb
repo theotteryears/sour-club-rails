984c015f7531089afb6730c9895858894111bc37
theotteryears <theotteryears@me.com>
2025-07-03 00:16:21 +0100

    add realtime updates

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,6 @@
 class Beer < ApplicationRecord
   has_one_attached :image
+
+  after_create_commit -> { broadcast_append_to "beers", target: "beer_list", partial: "beers/beer", locals: { beer: self } }
+  after_destroy_commit -> { broadcast_remove_to "beers" }
 end
