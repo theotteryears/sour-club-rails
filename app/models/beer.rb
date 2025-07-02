ab88d90c4e134f9606641f7f483b67d9f3e80b4d
theotteryears <theotteryears@me.com>
2025-07-03 00:37:38 +0100

    add realtime updates

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,9 @@
 class Beer < ApplicationRecord
   has_one_attached :image
 
-  after_create_commit -> { broadcast_append_to "beers", target: "beer_list", partial: "beers/beer", locals: { beer: self } }
+  after_create_commit -> {
+  beer_with_attachment = Beer.includes(image_attachment: :blob).find(id)
+  broadcast_prepend_to "beers", target: "beer_grid", partial: "beers/beer", locals: { beer: beer_with_attachment }
+}
   after_destroy_commit -> { broadcast_remove_to "beers" }
 end
