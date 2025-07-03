b7baf59cc7e3d93fbb62d2297f198ffadb4c7ca5
theotteryears <theotteryears@me.com>
2025-07-03 23:26:40 +0100

    add realtime updates

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,17 +3,7 @@ class Beer < ApplicationRecord
 
   after_create_commit -> {
   beer_with_attachment = Beer.includes(image_attachment: :blob).find(id)
-
-  image_url = Rails.application.routes.url_helpers.rails_blob_url(
-    beer_with_attachment.image,
-    host: Rails.application.routes.default_url_options[:host],
-    protocol: Rails.application.routes.default_url_options[:protocol]
-  )
-
-  broadcast_prepend_to "beers",
-    target: "beer_grid",
-    partial: "beers/beer",
-    locals: { beer: beer_with_attachment, image_url: image_url }
+  broadcast_prepend_to "beers", target: "beer_grid", partial: "beers/beer", locals: { beer: beer_with_attachment }
 }
   after_destroy_commit -> { broadcast_remove_to "beers" }
 end
