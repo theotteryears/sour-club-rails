0fd7b8f4a7f6821f5320199e1b5c141e49f81342
theotteryears <theotteryears@me.com>
2025-07-16 23:51:19 +0100

    add edit button to beer

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,6 @@
 class BeersController < ApplicationController
+  before_action :set_beer, only: [:edit, :update, :destroy]
+
   def index
     @beers = Beer.order(created_at: :desc)
   end
@@ -16,8 +18,19 @@ class BeersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @beer.update(beer_params)
+      redirect_to beers_path, notice: "Beer updated successfully."
+    else
+      flash.now[:alert] = "Failed to update the beer."
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
-    @beer = Beer.find(params[:id])
     @beer.destroy
 
     flash.now[:notice] = "Beer deleted successfully."
@@ -30,7 +43,11 @@ class BeersController < ApplicationController
 
   private
 
+  def set_beer
+    @beer = Beer.find(params[:id])
+  end
+
   def beer_params
-  params.require(:beer).permit(:name, :brewery, :sourness, :design, :jenesaisquoi, :image)
+    params.require(:beer).permit(:name, :brewery, :sourness, :design, :jenesaisquoi, :image)
   end
 end
--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,10 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["box"]
+
   connect() {
+    // Animate in
+    requestAnimationFrame(() => {
+      this.boxTarget.classList.remove("opacity-0", "-translate-y-5")
+      this.boxTarget.classList.add("opacity-100", "translate-y-0")
+    })
+
+    // Animate out after 3 seconds
     setTimeout(() => {
-      this.element.classList.add("opacity-0", "transition-opacity", "duration-500")
-      setTimeout(() => this.element.remove(), 500)
-    }, 2000)
+      this.boxTarget.classList.add("opacity-0", "-translate-y-5")
+      this.boxTarget.classList.remove("opacity-100", "translate-y-0")
+    }, 3000)
+
+    // Remove from DOM after transition
+    setTimeout(() => {
+      this.boxTarget.remove()
+    }, 3500)
   }
 }
